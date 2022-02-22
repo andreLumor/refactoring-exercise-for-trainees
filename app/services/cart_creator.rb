ff72e8a5192d9583bdcd9343fd0690d8f7858678
André Moreira <andre.moreira@codeminer42.com>
2022-02-22 18:41:10 -0300

    Create cart creator service

---
+++ app/services/cart_creator.rb
@@ -0,0 +1,11 @@
+class CartCreator
+  def self.call(purchase_params)#diminuir exposição desnecessária
+    cart = Cart.find_by(id: purchase_params[:cart_id])
+    return false unless cart
+    if cart.user.nil?
+      user_params = purchase_params[:user] ? purchase_params[:user] : {}
+      cart.user = User.create(**user_params.merge(guest: true))
+    end
+    return cart
+  end
+end
---
+++ spec/services/cart_creator_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe CartCreator, type: :model do
+  it "returns a cart if valid arguments"
+  it "returns error if cart doesnt exist"
+  it "creates user if cart doesnt have one"
+end
